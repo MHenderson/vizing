da8c301960a8cef7852b8226cd62e652e5225a5f
Matthew Henderson <matthew.james.henderson@gmail.com>
2011-01-02 15:40:45 +0000

    Fixed testing of independent sets. Exact decision implemented.

--- vizing/test_functions.py
+++ vizing/test_functions.py
@@ -11,19 +11,29 @@ EXAMPLES:
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
 #                  http://www.gnu.org/licenses/
-#********************************************************************************
+#********************************************************************************   
 
+import itertools
 from networkx import graph_clique_number
 
 def is_independent(graph, nodes):
     """Decides whether of not the subgraph of 'graph' induced by nodes in 
     'nodes' is an independent set or not."""
-    return graph_clique_number(graph.subgraph(nodes))==1
+    if len(nodes) == 0:
+        return True
+    else:
+        return graph_clique_number(graph.subgraph(nodes)) == 1
+
+def is_exact(graph, colouring):
+    """Decides whether exactly every vertex of 'graph' is coloured."""
+    coloured_vertices = list(itertools.chain.from_iterable(colouring.values()))
+    coloured_vertices.sort()
+    return coloured_vertices == graph.nodes()
 
 def is_proper(graph, colouring):
     """Decides whether or not 'colouring' is a proper colouring of the vertices
     of 'graph'."""
-    return all(map(lambda nodes: is_independent(graph, nodes), colouring.values()))
+    return is_exact(graph, colouring) and all(map(lambda nodes: is_independent(graph, nodes), colouring.values()))
 
 def has_colour(list_assignment, vertex, colour):
     """Decides whether or not 'colour' is a member of the list assigned to 

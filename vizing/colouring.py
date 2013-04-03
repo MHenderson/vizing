5aa590bad8314725d56c04083a3a3440b814713d
Matthew Henderson <matthew.james.henderson@gmail.com>
2013-04-03 12:09:54 +0100

    Imported colouring components from sudoku.py.

--- vizing/colouring.py
+++ vizing/colouring.py
@@ -17,6 +17,8 @@ AUTHORS:
 #                  http://www.gnu.org/licenses/
 #********************************************************************************
 
+import random
+
 from vizing.models.pythonconstraint import _CP_model_
 #from vizing.models.ortools import _or_CP_model_
 
@@ -57,4 +59,89 @@ def list_colouring(graph, list_assignment, model = 'CP'):
     else:
         raise Exception('No such model')
 
+####################################################################
+# Vertex coloring algorithms
+####################################################################
+
+def neighboring_colors(graph, node):
+    """Returns list of colors used on neighbors of 'node' in 'graph'."""
+    return filter(None, [graph.node[neighbor].get('color') for neighbor in graph.neighbors(node)])
+
+def n_colors(graph):
+    """The number of distinct colors used on vertices of 'graph'."""
+    return len(set([graph.node[i]['color'] for i in graph.nodes()]))
+
+def least_missing(colors):
+    """The smallest integer not in 'colors'."""
+    colors.sort()
+    for color in colors:
+        if color + 1 not in colors:
+            return color + 1
+
+def first_available_color(graph, node):
+    """The first color not used on neighbors of 'node' in 'graph'."""
+    used_colors = neighboring_colors(graph, node)
+    if len(used_colors) == 0:
+        return 1
+    else:
+        return least_missing(used_colors)
+
+def saturation_degree(graph, node):
+    """Saturation degree of 'node' in 'graph'."""
+    return len(set(neighboring_colors(graph, node)))
+
+class FirstAvailableColor():
+    """First available color choice visitor."""
+
+    def __call__(self, graph, node):
+        return first_available_color(graph, node)
+
+class InOrder():
+    """Natural vertex ordering strategy."""
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def __iter__(self):
+        return self.graph.nodes_iter()
+
+class RandomOrder():
+    """Random vertex ordering strategy."""
+
+    def __init__(self, graph):
+        self.graph = graph
+        self.nodes = self.graph.nodes()
+
+    def __iter__(self):
+        random.shuffle(self.nodes)
+        return iter(self.nodes)
+
+class DSATOrder():
+    """Saturation degree vertex ordering strategy."""
+
+    def __init__(self, graph):
+        self.graph = graph
+        self.nodes = self.graph.nodes()
+        self.value = 0
+
+    def dsatur(self, node):
+        return saturation_degree(self.graph, node)
+
+    def next(self):
+        self.value += 1
+        if self.value > self.graph.order(): raise StopIteration
+        self.nodes.sort(key = self.dsatur)
+        return self.nodes.pop()
+
+    def __iter__(self):
+        return self
+
+def vertex_coloring(graph, nodes = InOrder, choose_color = FirstAvailableColor):
+    """Generic vertex coloring algorithm. Node ordering specified by 'nodes'
+    iterator. Color choice strategy specified by 'choose_color'."""
+    nodes = nodes(graph)
+    for node in nodes:
+        if not graph.node[node].get('color'):
+            graph.node[node]['color'] = choose_color()(graph, node)
+    return graph
 

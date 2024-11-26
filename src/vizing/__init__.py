3aa3358405f763b3b76abc56e0d5f5e0db800cf6
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-11-26 09:50:43 +0000

    Add tests. Rename files and functions.

--- src/vizing/__init__.py
+++ src/vizing/__init__.py
@@ -1,6 +1,6 @@
 import constraint
 
-def list_colouring_problem(G):
+def node_list_colouring_problem(G):
   """
   Return a constraint problem representing a node list-colouring instance.
 
@@ -14,14 +14,14 @@ def list_colouring_problem(G):
     P.addConstraint(constraint.AllDifferentConstraint(), edge)
   return(P)
 
-def list_colouring_solution(G):
+def node_list_colouring_solution(G):
   """
   Return a list-coloured graph.
 
   :param G: A graph with lists of permissible colours assigned to nodes.
   :return A properly list-coloured graph.
   """
-  P = list_colouring_problem(G)
+  P = node_list_colouring_problem(G)
   S = P.getSolution()
   for node in S:
     G.nodes[node]['colour'] = S[node]
